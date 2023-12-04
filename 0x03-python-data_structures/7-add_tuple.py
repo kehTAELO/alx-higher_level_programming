091731f4afb64d5fc3219d3f99aab499958ecc9a
kehtaelo <sylvestertaelo97@gmail.com>
2023-12-04 11:04:49 -0800

    3rd

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    i = tuple_a
+    j = tuple_b
+
+    if i in tuple_a < 2 != 0:
+        return None
+    elif j in tuple_b > 2:
+        tuple_b[0:1]
+    else:
+        print(tuple_a, tuple_b, end=" ,")
