b36ded8568bd34ce3d275603e7ae24f59f1f3a4f
kehtaelo <sylvestertaelo97@gmail.com>
2024-04-30 07:52:33 -0700

    1-rectangle.py

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -5,11 +5,19 @@ class Rectangle:
         self.height = height
     @property
     def width(self):
-        "this method returns value of width"
+        """this method returns value of height
+returns:
+height of the rectangle"""
         return self.__width
     @width.setter
     def width(self,value):
-        "method will define height and also raise Errors"
+        """method defines width
+        Args:
+        value: width
+        raises:
+            TypeError:widht must be an integer
+            ValueError:width must be >= 0
+        """
         if type(value) is not int:
             raise TypeError("width must be an integer")
         if value < 0:
@@ -17,11 +25,19 @@ class Rectangle:
         self.__width = value
 
 @property
-"this method returns value of height"
 def height(self):
+    """this method returns value of height
+returns:
+height of the rectangle"""
     return self.__height
 @height.setter
-"method defines height and raises typeError and ValueError"
+"""method defines height 
+Args:
+value: height
+raises:
+    TypeError:height must be an integer
+    ValueError:height must be >= 0
+"""
 def height(self,value):
     if type(value) != int:
         raise TypeError("height must be an integer")
