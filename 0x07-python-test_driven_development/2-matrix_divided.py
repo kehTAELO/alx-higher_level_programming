ed667c7ee69a9ea71e2baa93072cf1b0524836f3
kehtaelo <sylvestertaelo97@gmail.com>
2024-04-30 01:38:04 -0700

    0-add_integer.py

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 def matrix_divided(matrix, div):
-    if matrix != [int,float] 
-    raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-if div != int and != float
-raise TypeError("div must be a number")
-if div != = 0:
-    raise ZeroDivisionError("division by zero")
-else:
-    if div is float:
-        div = float(div)
-        if div is int:
-            div = int(div)
-        return matrix /2
+    if matrix != [float,int]:
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    if [int] == matrix:
+        raise TypeError("Each row of the matrix must have the same size")
+    if div != int and div is not float:
+        raise TypeError("div must be a number")
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+    if div == int:
+        div = int(div)
+        if div == float :
+            div = float(div)
+            return matrix
