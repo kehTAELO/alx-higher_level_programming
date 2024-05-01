00c545e02d4dccbff66f931c47b6c7beaa278d97
kehtaelo <sylvestertaelo97@gmail.com>
2024-05-01 12:22:56 -0700

    6

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,17 +1,5 @@
 #!/usr/bin/python3
+
 def read_file(filename=""):
-    """"
-    This function reads a text file (UTF8) and prints it to stdout.
-
-    Args:
-        filename (str): The name of the file to be read. Default is an empty string.
-    """
-
-    """ Open the file in read mode ('r') with UTF-8 encoding."""
-    with open('my_file_0.txt', 'r', encoding='utf-8') as file:
-        """ Read the entire content of the file"""
-        file_contents = file.read()
-
-        """ Print the content to stdout."""
-        print(file_contents, end="")
-
+    with open(filename, "r", encoding="UTF-8") as f:
+        print(f.read(), end="")
