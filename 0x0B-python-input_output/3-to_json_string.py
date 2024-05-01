447afa053a24cc476469afbaf891a947e0131ee7
kehtaelo <sylvestertaelo97@gmail.com>
2024-05-01 13:20:01 -0700

    3

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-
-
+"""this will Define a string-to-JSON function."""
 import json
 
 
 def to_json_string(my_obj):
+    """this function will Return the JSON representation."""
     return json.dumps(my_obj)
