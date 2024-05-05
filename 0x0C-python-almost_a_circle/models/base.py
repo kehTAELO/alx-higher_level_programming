72c92a338e1e6652278132e7252effc072114e4d
kehtaelo <sylvestertaelo97@gmail.com>
2024-05-05 01:46:41 -0700

    0&1

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""this are the models for the base class"""
+
+class Base:
+    """the private attribute"""
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        self.id = id
+
+        if id != None:
+            self.id = id
+        else:
+            Base__nb_objects = + 1
+            self.id = Base.__nb_objects
